0791858ccaca2f290c331a97b4d98c858c73398f
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-23 23:33:55 +0600

    more resume data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,18 +16,18 @@ export default function Home() {
     <main className="bg-cover" style={{ backgroundImage: `url(${BgMain})` }}>
       <NavBar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-Powered feedback</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+            ))}
+          </div>
+        )}
       </section>
-      {resumes.length > 0 && (
-        <div className="resumes-section">
-          {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume}></ResumeCard>
-          ))}
-        </div>
-      )}
     </main>
   );
 }
