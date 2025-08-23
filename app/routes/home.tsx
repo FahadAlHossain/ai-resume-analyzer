b112ec65d999823ea9c3082c23908eb3a2f10f79
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-23 23:30:06 +0600

    resume card added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import BgMain from "/assets/public/images/bg-main.svg";
+import NavBar from "~/components/NavBar";
+import { resumes } from "./../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeAi" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-cover" style={{ backgroundImage: `url(${BgMain})` }}>
+      <NavBar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-Powered feedback</h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
